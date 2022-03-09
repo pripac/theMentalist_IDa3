3d0d8db8e9b2c5db03dfceaac9a05242808b5ca6
pripac <pristudy96@gmail.com>
2022-03-09 17:54:24 -0300

    script working

---
+++ theMentalist_IDa3.js
@@ -0,0 +1,20 @@
+
+function Guess(){   
+var secretNumber = 3
+var buttonIntegration = parseInt(document.getElementById("value").value)
+
+if (secretNumber === buttonIntegration){ 
+    alert("You won!") 
+} else {
+    alert("You lost.")
+} 
+}
+
+
+  
+
+
+
+
+
+//   if (secretNumber === inputValue){ alert("You won!") }else {alert("You lost.")} 
